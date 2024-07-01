2c8a411bdd11d661abd995b9a242c14619cde9ea
Vu Duc Hieu Cao <121604920+hieucao240402@users.noreply.github.com>
2024-07-01 13:44:44 +0700

    upadte 1

---
+++ prf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>;
+#include<string.h>;
+
+
+int main(){
+    // create Name array || description array
+    char name[10];
+    char description[10];
+    
+
+
+    return 0;
+}
+
+
+
